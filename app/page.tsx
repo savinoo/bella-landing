15205689da229f202f58ebfd38d3dd368163227d
Lucas Lorenzo Savino <lulosavino@gmail.com>
2026-05-29 23:00:27 +0000

    landing: insere funil apos o Hero e move video pro fim

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import { Hero } from "@/components/Hero"
-import { VideoSection } from "@/components/VideoSection"
+import { FunilCompleto } from "@/components/FunilCompleto"
 import { ValueBullets } from "@/components/ValueBullets"
 import { ComoFunciona } from "@/components/ComoFunciona"
 import { FAQ } from "@/components/FAQ"
 import { Pricing } from "@/components/Pricing"
+import { VideoSection } from "@/components/VideoSection"
 import { LiveDemo } from "@/components/LiveDemo"
 import { Footer } from "@/components/Footer"
 
@@ -11,11 +12,12 @@ export default function Home() {
   return (
     <main className="flex flex-col">
       <Hero />
-      <VideoSection />
+      <FunilCompleto />
       <ValueBullets />
       <ComoFunciona />
       <FAQ />
       <Pricing />
+      <VideoSection />
       <LiveDemo />
       <Footer />
     </main>
